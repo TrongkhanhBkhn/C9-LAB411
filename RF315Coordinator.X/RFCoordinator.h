76d1c3f17d09d6ea0b8affb6c415a8343d7a337d
TrongKhanhBkhn <you@example.com>
2014-06-27 21:32:40 +0700

    first commit

---
+++ RF315Coordinator.X/RFCoordinator.h
@@ -0,0 +1,30 @@
+/* 
+ * File:   RFRounter.h
+ * Author: trongkhanh
+ *
+ * Created on May 19, 2014, 1:20 PM
+ */
+
+#ifndef RFCOORDINATOR_H
+#define	RFCOORDINATOR_H
+
+#include "CC1101/CC1101.h"
+#include "LCD/LCD.h"
+#include "SPI/SPI.h"
+#include "Hardware/Generic.h"
+#include "TIMER/TIMER.h"
+#include "UART/UART.h"
+#include "RFConfig.h"
+
+
+unsigned char chip_status_rx, chip_status_tx;
+unsigned char buffer[48];
+unsigned char  Ok;
+
+#define DEBUG_UART
+void Timer_Isr(void);
+void SendData(unsigned char data[], unsigned char addr,  unsigned char command);
+void OSC_config(void);
+
+#endif	/* RFROUNTER_H */
+
